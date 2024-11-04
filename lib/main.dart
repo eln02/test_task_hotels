1e5a523deef0ecbee283db7badfc24c9a6b33d39
eln02 <drannikovaelena6@gmail.com>
2024-11-04 05:08:23 +0300

    implement DI with get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:test_task_hotels/data/api/hotel_api.dart';
 import 'package:test_task_hotels/data/api/reservation_api.dart';
 import 'package:test_task_hotels/data/api/room_api.dart';
@@ -14,29 +15,60 @@ import 'package:test_task_hotels/presentation/bloc/room_bloc.dart';
 import 'package:test_task_hotels/presentation/bloc/tourist_forms_bloc.dart';
 import 'package:test_task_hotels/presentation/view/all_hotels_page/hotels_page.dart';
 import 'data/repositories/hotel_repository_impl.dart';
+import 'domain/repositories/hotel_repository.dart';
+import 'domain/repositories/reservation_repository.dart';
+import 'domain/repositories/room_repository.dart';
 import 'domain/use_cases/get_all_hotels.dart';
 
+final getIt = GetIt.instance;
+
+void setupLocator() {
+  getIt.registerSingleton<Dio>(Dio());
+
+  getIt.registerLazySingleton(() => HotelApi(getIt<Dio>()));
+  getIt.registerLazySingleton(() => RoomApi(getIt<Dio>()));
+  getIt.registerLazySingleton(() => ReservationApi(getIt<Dio>()));
+
+  getIt.registerLazySingleton<HotelRepository>(() => HotelRepositoryImpl(getIt<HotelApi>()));
+  getIt.registerLazySingleton<RoomRepository>(() => RoomRepositoryImpl(getIt<RoomApi>()));
+  getIt.registerLazySingleton<ReservationRepository>(() => ReservationRepositoryImpl(getIt<ReservationApi>()));
+
+  getIt.registerLazySingleton(() => GetAllHotelsUseCase(getIt<HotelRepository>()));
+  getIt.registerLazySingleton(() => GetRoomsByIdUseCase(getIt<RoomRepository>()));
+  getIt.registerLazySingleton(() => GetReservationByIdUseCase(getIt<ReservationRepository>()));
+
+  getIt.registerFactory(() => HotelBloc(getIt<GetAllHotelsUseCase>()));
+  getIt.registerFactory(() => RoomBloc(getIt<GetRoomsByIdUseCase>()));
+  getIt.registerFactory(() => ReservationBloc(getIt<GetReservationByIdUseCase>()));
+  getIt.registerFactory(() => TouristFormsBloc());
+}
+
 void main() {
-  runApp(MyApp());
+  setupLocator();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final getAllHotelsUseCase =
-      GetAllHotelsUseCase(HotelRepositoryImpl(HotelApi(Dio())));
-  final getRoomsByIdUseCase =
-      GetRoomsByIdUseCase(RoomRepositoryImpl(RoomApi(Dio())));
-  final getReservationByIdUseCase = GetReservationByIdUseCase(
-      ReservationRepositoryImpl(ReservationApi(Dio())));
-
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(providers: [
-      BlocProvider(create: (context) => HotelBloc(getAllHotelsUseCase)..add(LoadHotelsEvent())),
-      BlocProvider(create: (context) => RoomBloc(getRoomsByIdUseCase)),
-      BlocProvider(create: (context) => ReservationBloc(getReservationByIdUseCase)),
-      BlocProvider(create: (context) => TouristFormsBloc())
-    ], child: const MaterialApp(home: AllHotelsPage()));
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<HotelBloc>()..add(LoadHotelsEvent()),
+        ),
+        BlocProvider(
+          create: (context) => getIt<RoomBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<ReservationBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<TouristFormsBloc>(),
+        ),
+      ],
+      child: const MaterialApp(home: AllHotelsPage()),
+    );
   }
 }
